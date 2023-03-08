0db458743ef9666811bb965513592c7ecf334880
EstebanMontalvo99 <estebanmontalvo99@hotmail.com>
2023-03-07 19:44:43 -0500

    Perfeccionando el top y nav y agregando  ptoducts

--- assets/Js/main.js
+++ assets/Js/main.js
@@ -1,4 +1,60 @@
-const nav = document.querySelector("nav");
+async function getProducts() {
+  try {
+    const data = await fetch(
+      "https://ecommercebackend.fundamentos-29.repl.co/"
+    );
+    const response = await data.json();
+    window.localStorage.setItem("products", JSON.stringify(response));
+    return response;
+  } catch (error) {
+    console.log(error);
+  }
+}
+function printProducts(db) {
+  const productsHTML = document.querySelector(".products");
+  let html = ``;
+  for (const product of db.products) {
+    html += `
+    <div class="product">
+      <div class="productImg">
+        <img src="${product.image}" alt="Imagen">
+      </div>
+
+      <div class="productInfo">
+        <h4>${product.name} <span><b>Stock</b>: ${product.quantity}</span> </h4>
+        <h5>
+           $${product.price} 
+          <i class='bx bx-plus' id="${product.id}"></i>
+        </h5>
+        </div>
+      </div>`;
+  }
+
+  productsHTML.innerHTML = html;
+}
+function handlerPrintAmountProducts(db) {
+  const amountProductsHTML = document.querySelector(".amountProducts");
+  let amount = 0;
+  for (const product in db.cart) {
+    amount += db.cart[product].amount;
+  }
+  amountProductsHTML.textContent = amount;
+}
+function handleTotal(db) {
+  handlerPrintAmountProducts(db); //Esto es del ultimo video que ya viste va al ultimo
+}
+async function main() {
+  const db = {
+    products:
+      JSON.parse(window.localStorage.getItem(`products`)) ||
+      (await getProducts()),
+    cart: {},
+  };
+  printProducts(db);
+  handlerPrintAmountProducts(db);
+}
+main();
+const header = document.querySelector("header");
 const menu = document.querySelector(".menu");
 const bxMenu = document.querySelector(".bxMenu");
 const darkMode = document.querySelector(".darkMode");
@@ -13,7 +69,7 @@ bxMenu.addEventListener(`click`, function () {
 closeMenu.addEventListener(`click`, function () {
   menu.classList.toggle("menu_show");
   closeMenu.replaceWith(bxMenu);
-  darkMode.style.display = "flex";
+  darkMode.style.display = "block";
 });
 
 menu.addEventListener(`click`, function () {
@@ -25,9 +81,9 @@ menu.addEventListener(`click`, function () {
 window.addEventListener("scroll", function () {
   if (window.scrollY > 50) {
     // Add the "headerscrolled" class to the "nav" element
-    nav.classList.add("headerscrolled");
+    header.classList.add("headerscrolled");
   } else {
     // Remove the "headerscrolled" class from the "nav" element
-    nav.classList.remove("headerscrolled");
+    header.classList.remove("headerscrolled");
   }
 });
